81fabfd7ead6ae69c7b97b88fcbc9266746472df
Mazid70 <mazidurmazid431@gmail.com>
2024-03-26 21:23:12 +0600

    graph-chart added

---
+++ src/components/PageToRead.jsx
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+import { useLoaderData } from "react-router-dom";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+const getPath = (x, y, width, height) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${
+    y + height / 3
+  }
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+    x + width
+  }, ${y + height}
+  Z`;
+};
+
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
+const PageToRead = () => {
+    const loadData=useLoaderData()
+  return (
+    <BarChart
+    className="mx-auto bg-[#13131308] p-10 rounded-xl mt-10"
+      width={1200}
+      height={756}
+      data={loadData}
+      margin={{
+        top: 20,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      
+      <XAxis dataKey="bookName" />
+      <YAxis />
+      <Bar
+        dataKey="totalPages"
+        fill="#8884d8"
+        shape={<TriangleBar />}
+        label={{ position: "top" }}
+      >
+        {loadData.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+        ))}
+      </Bar>
+    </BarChart>
+  );
+};
+TriangleBar.propTypes = {
+    fill: PropTypes.string,
+    x: PropTypes.number,
+    y: PropTypes.number,
+    width: PropTypes.number,
+    height: PropTypes.number,
+  };
+export default PageToRead;
--- src/main.jsx
+++ src/main.jsx
@@ -8,31 +8,33 @@ import App from "./App";
 import BookDetails from "./components/BookDetails";
 import ListedBook from "./components/ListedBook";
 import ErroePage from "./ErroePage";
-
+import PageToRead from "./components/PageToRead";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    errorElement:<ErroePage></ErroePage>,
+    errorElement: <ErroePage></ErroePage>,
     element: <App></App>,
     children: [
       {
         path: "/",
         element: <Home></Home>,
       },
-      {loader:()=>fetch("../Books.json"),
+      {
+        loader: () => fetch("../Books.json"),
         path: "/listed",
-        element:<ListedBook></ListedBook>
+        element: <ListedBook></ListedBook>,
       },
       {
-        path:"/read"
-      }
-      ,
+        loader: () => fetch("../Books.json"),
+        path: "/read",
+        element: <PageToRead></PageToRead>,
+      },
       {
-        loader:()=>fetch("../Books.json"),
-        path:"/book/:bookId",
-        element:<BookDetails></BookDetails>
-      }
+        loader: () => fetch("../Books.json"),
+        path: "/book/:bookId",
+        element: <BookDetails></BookDetails>,
+      },
     ],
   },
 ]);
